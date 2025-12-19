38106337f2a0992cc342c3b1b25ad82608cb9627
moksh555 <57554299+moksh555@users.noreply.github.com>
2025-12-19 15:26:52 -0600

    final changes to CORS

--- src/app/main.py
+++ src/app/main.py
@@ -31,7 +31,7 @@ def create_app() -> FastAPI:
     if settings.CORS_ORIGINS:
         app.add_middleware(
             CORSMiddleware,
-            allow_origins=["http://localhost:5173", "https://the-llm-court-frontend.vercel.app", "https://llm-court.com",],
+            allow_origins=["http://localhost:5173", "https://the-llm-court-frontend.vercel.app", "https://llm-court.com","https://the-llm-court-frontend-72flm8q0x-moksh-vaghasias-projects.vercel.app/"],
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
